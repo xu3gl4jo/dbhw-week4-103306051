13083c641f25f1a0921c6f0c5998760ccc9227d2
willy <103306052@nccu.edu.tw>
2016-04-08 12:00:17 +0800

    first commit

---
+++ hw4.js
@@ -0,0 +1,19 @@
+var API_PATH = "http://data.taipei/opendata/datalist/apiAccess?scope=resourceAquire&rid=bf073841-c734-49bf-a97f-3757a6013812";
+
+function getData(cb) {
+  $.ajax({
+    url : API_PATH,
+    method : "GET",
+    success : function(data) {
+      console.log(data);
+      cb(null, data);
+    },
+    error : function(err) {
+      cb(err);
+    }
+  });
+}
+
+function viewdetails(){
+    items = items + "<li class='list-group-item'>" + key + ":" + spot[key] + "</li>";
+}
